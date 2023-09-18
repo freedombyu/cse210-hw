fe41a4b4d58df86ebfc8f72d710cb2e96f5520e0
freedombyui <131212585+freedombyu@users.noreply.github.com>
2023-09-18 13:41:29 +0200

    added another comment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
+    // This program will ask the user for a series of numbers,
+    // When the user type 0.
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers entered.");
+        }
+        else
+        {
+            int sum = numbers.Sum();
+            float average = ((float)sum) / numbers.Count;
+            int largest = numbers.Max();
+            int smallestPositive = numbers.Where(x => x > 0).Min();
+            List<int> sortedNumbers = numbers.OrderByDescending(x => x).ToList();
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+            Console.WriteLine("The sorted list is:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
     }
 }
+
+
