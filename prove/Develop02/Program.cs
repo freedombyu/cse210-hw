020fe788859801a215321cea867d5a8b6af57fd2
freedombyui <131212585+freedombyu@users.noreply.github.com>
2023-09-25 23:31:09 +0200

    added a statment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,139 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+class JournalEntry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public DateTime Date { get; set; }
+
+    public JournalEntry(string prompt, string response, DateTime date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+}
+
+class Journal
+{
+    private List<JournalEntry> entries = new List<JournalEntry>();
+
+    public void AddEntry(JournalEntry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine($"Date: {entry.Date}");
+            Console.WriteLine($"Prompt: {entry.Prompt}");
+            Console.WriteLine($"Response: {entry.Response}");
+            Console.WriteLine(new string('-', 20));
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine($"Date: {entry.Date}");
+                writer.WriteLine($"Prompt: {entry.Prompt}");
+                writer.WriteLine($"Response: {entry.Response}");
+                writer.WriteLine(new string('-', 20));
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        entries.Clear();
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string line;
+            JournalEntry entry = null;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith("Date: "))
+                {
+                    DateTime date = DateTime.Parse(line.Substring(6));
+                    string prompt = reader.ReadLine().Substring(8);
+                    string response = reader.ReadLine().Substring(10);
+                    reader.ReadLine(); // Skip the separator line
+                    entry = new JournalEntry(prompt, response, date);
+                    entries.Add(entry);
+                }
+            }
+        }
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        List<string> prompts = new List<string>
+        {
+            "Who were the most fascinating persons I spoke with during the past week? ",
+            "What happened in my life during the past month that I will never forget? ",
+            "What proof of the Lord's interveWhat was my I experience yesterday that was the strongest? ",
+            "What did I observe this week that changed my life? ",
+            "If I could go back in time today, what would I do differently? ",
+        };
+
+        while (true)
+        {
+            Console.WriteLine("\nJournal Entry Application Menu:");
+            Console.WriteLine("1. Create a new journal entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = prompts[new Random().Next(prompts.Count)];
+                    Console.WriteLine(prompt);
+                    Console.Write("Your response: ");
+                    string response = Console.ReadLine();
+                    DateTime date = DateTime.Now;
+                    JournalEntry entry = new JournalEntry(prompt, response, date);
+                    journal.AddEntry(entry);
+                    Console.WriteLine("Entry created successfully!");
+                    break;
+                case "2":
+                    Console.WriteLine("\nJournal Entries:");
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter the filename to save the journal: ");
+                    string saveFilename = Console.ReadLine();
+                    journal.SaveToFile(saveFilename);
+                    Console.WriteLine($"Journal saved to {saveFilename}.");
+                    break;
+                case "4":
+                    Console.Write("Enter the filename to load the journal: ");
+                    string loadFilename = Console.ReadLine();
+                    journal.LoadFromFile(loadFilename);
+                    Console.WriteLine($"Journal loaded from {loadFilename}.");
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
+
