a0f02ef9a9e56d49ea64b3ceff05a9c2d5c125ac
freedombyui <131212585+freedombyu@users.noreply.github.com>
2023-09-29 01:42:46 +0200

    add a return value

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,51 @@
 using System;
 
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        try
+        {
+            // Create fractions
+            Fraction fraction1 = new Fraction(3, 4);
+            Fraction fraction2 = new Fraction(1, 2);
+            Fraction fraction3 = new Fraction(2, 5);
+
+            // Display fraction strings and decimal values
+            Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()} = {fraction1.GetDecimalValue()}");
+            Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()} = {fraction2.GetDecimalValue()}");
+            Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()} = {fraction3.GetDecimalValue()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 }
